a00630f5c560296b50fd8e113b9e62928731c6b6
Michael Bulpitt <michael@kashoo.co.uk>
2015-06-19 11:12:22 +0100

    remove warning

--- addon/components/select-2.js
+++ addon/components/select-2.js
@@ -350,16 +350,6 @@ var Select2Component = Ember.Component.extend({
 
       if (unmatchedValues === 0) {
         self.set('_hasSelectedMissingItems', false);
-      } else {
-        // disable the select2 element if there are keys left in the values
-        // array that were not matched to an object
-        self.set('_hasSelectedMissingItems', true);
-
-        Ember.warn("select2#initSelection was not able to map each \"" +
-          optionValuePath +"\" to an object from \"content\". The remaining " +
-          "keys are: " + values + ". The input will be disabled until a) the " +
-          "desired objects is added to the \"content\" array or b) the " +
-          "\"value\" is changed.", !values.length);
       }
 
       if (multiple) {
